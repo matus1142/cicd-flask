4d94cd1279faa8b566af66a95c014c4ba6c9b6e1
Matus Lhongpol <matuslhongpol@gmail.com>
2024-10-13 00:03:44 +0700

    test fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,5 +6,5 @@ from app import app as _app
 def test_hello():
     client = _app.test_client()
     response = client.get('/')
-    assert response.data == b'Hello, World!!!'
+    assert response.data == b'Hello, World!'
     assert response.status_code == 200
